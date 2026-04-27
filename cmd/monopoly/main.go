4eb015ff55a376ca02b3cc76c83589bfea701bc0
moohalem <mohalem.public@gmail.com>
2026-04-27 14:17:48 +0700

    Initial commit: First Versioncd

---
+++ cmd/monopoly/main.go
@@ -0,0 +1,16 @@
+package main // Fixed: Lowercase 'package'
+
+import (
+	"fmt"
+
+	"monopoly/internal/game"
+	"monopoly/internal/ui"
+)
+
+func main() {
+	ui.ClearScreen()
+	fmt.Println("")
+	playerCount := game.Welcome()
+	players := game.NewPlayers(playerCount)
+	game.Play(players)
+}
